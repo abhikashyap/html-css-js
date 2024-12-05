e0ff7aa94d8edf18f59598b802f7ac0c60bf27b6
abhikashyap <abhikashyap.ds@gmail.com>
2024-12-05 16:32:55 +0530

    added project1

--- heart trail animation/index.js
+++ heart trail animation/index.js
@@ -12,15 +12,23 @@ body_element.addEventListener("mousemove", (e) =>{
         return color;
     };
 
+    const get_random_size= (e) =>{
+        let size=Math.floor(Math.random()*50)
+        return size
+
+    };
+
 
     const text_color=get_random_color();
     const new_element=document.createElement('span');
-    new_element.textContent='*'
+    new_element.textContent='*';
     new_element.style.position = 'absolute'; 
     new_element.style.left = `${e.offsetX}px`; 
     new_element.style.top =`${e.offsetY}px`
     new_element.style.color=text_color;
+    new_element.style.fontSize=`${get_random_size()}px`;
     body_element.appendChild(new_element);
+
 
     setTimeout(() => {
         body_element.removeChild(new_element)
